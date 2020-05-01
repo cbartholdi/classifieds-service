ed3252a24f21081a95658aa01ccbe3abcc14f24a
Caroline Bartholdi <caroline.bartholdi@hotmail.com>
2020-05-01 19:39:36 +0200

    responses constants

--- api/classifieds/constants.py
+++ api/classifieds/constants.py
@@ -1,7 +1,20 @@
+from enum import Enum
+
+
 SORT_PRICE = 'price'
 SORT_CREATED = 'created'
 
+ASC_SIGNAL = 'asc'
+DESC_SIGNAL = 'desc'
+
+
 ORDERING_MAPPER = {
-    'asc': '',
-    'desc': '-',
+    ASC_SIGNAL: '',
+    DESC_SIGNAL: '-',
 }
+
+
+class Responses(Enum):
+    NOT_FOUND = 'NotFound'
+    INTERNAL_ERROR = 'InternalError'
+    ILLEGAL_ARGUMENT = 'IllegalArgument'
